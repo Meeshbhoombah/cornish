d57b997b404454588f66317f6d02d89ebc24eeb9
Rohan Mishra <rohanmishra2017@gmail.com>
2021-03-25 11:03:50 -0700

    Added hyper and hyper_static to serve client

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 
 use tokio::net::{TcpListener, TcpStream};
 
-//const STATIC_FILES: ;
+const STATIC_FILES: &str = "../../client/";
 const DEFAULT_ADDRESS: &str = "127.0.0.1:3000";
 
 async fn handle_connection(conn_stream: TcpStream, conn_addr: SocketAddr) {
@@ -11,11 +11,7 @@ async fn handle_connection(conn_stream: TcpStream, conn_addr: SocketAddr) {
 
 #[tokio::main]
 async fn main() {
-    // TODO take in Command Line Arugments
-    // mode (development/testing/production)
-    // optionally - path to config file
-    // TODO read in config from directory or file
-    // TODO serve a websocket with the given configuration 
+    // TODO rewrite with clap
     let bind_socket = TcpListener::bind(DEFAULT_ADDRESS).await;
     let listener = bind_socket.expect("Unable to bind to socket.");
     println!("Listening...");
