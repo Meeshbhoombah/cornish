862d5d3b34be7e3c5f9568aadbf901176a89ff78
Rohan Mishra <rohanmishra2017@gmail.com>
2021-03-26 09:26:29 -0700

    Added TCP listener

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,17 +2,27 @@ use std::path::Path;
 use std::net::SocketAddr;
 
 use futures::future;
+use tokio::net::{TcpListener, TcpStream};
 use hyper::Server;
 use hyper::service::{make_service_fn, service_fn};
 use hyper_staticfile::Static;
 
 mod routes;
 
-const CLIENT: &str = "../client/";
+const CLIENT: &str = "../client/index.html";
 const DEFAULT_ADDRESS: &str = "127.0.0.1:3000";
 
 #[tokio::main]
 async fn main() {
+    let addr: SocketAddr = DEFAULT_ADDRESS
+        .parse()
+        .expect("Failed to parse address.");
+
+    let bind_socket = TcpListener::bind(&addr).await;
+    let listener = bind_socket.expect("Unable to bind to socket.");
+    println!("Listening at: {}", &addr);
+
+    /*
     let client = Static::new(Path::new(CLIENT));
     let cornish = make_service_fn(|_| {
         // to make a reference to client available inside of the `service_fn`
@@ -29,5 +39,6 @@ async fn main() {
     
     server.await
         .expect("Server failed");
+    */
 }
 
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -6,22 +6,6 @@ use hyper::{Body, Request, Response};
 use hyper_staticfile::Static;
 
 pub async fn handle<B>(req: Request<B>, static_: Static) -> Result<Response<Body>, IoError> {
-
-    /*
-    match req.uri().path {
-        "/pgk/" => {
-            static_.clone().serve(req.await)
-        },
-        "/connect" => {
-            println!("TCP Connection");
-        },
-        _ => {
-            static_.clone().serve(req.await)
-        }
-    }
-    */
-
-
     if req.uri().path() == "/" {
         let res = ResponseBuilder::new()
             .status(StatusCode::MOVED_PERMANENTLY)
