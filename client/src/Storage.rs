1c11bfcc2333897cf784442da85368d2f9c9d282
Rohan Mishra <rohanmishra2017@gmail.com>
2021-03-31 08:27:43 -0700

    Restarting client

--- client/src/Storage.rs
+++
@@ -1,9 +0,0 @@
-// client/Storage
-// src/client/src/Storage.rs
-//
-// Retrives the domain localStorage object.
-//
-
-use seed::{prelude::*, *};
-
-
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,59 +1,21 @@
-// client
-// src/lib.rs
-//
-
 #![allow(
     clippy::wildcard_imports,
 )]
 
 use seed::{prelude::*, *};
 
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+struct Window {
+    connection: &'a &str,
 }
 
-type Model = i32;
-
-enum Msg {
-    Increment,
-}
-
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
-    match msg {
-        Msg::Increment => *model += 1,
+fn init(_: Url) -> Window {
+    // TODO establish real time connection with main server
+    Window {
+        connection: "Test"
     }
 }
 
-fn view(model: &Model) -> Node<Msg> {
-    div![
-        C!["counter"],
-        "This is a counter: ",
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
-    ]
-}
-
-#[wasm_bindgen(start)]
 pub fn start() {
-    App::start("app", init, update, view);
-}
-
-/*
-struct Client {
-}
-
-fn init(_: Url) -> Client {
-    // TODO establish real time connection with main server
-}
-
-struct Msg {
-    SignIn(User),
-    LogOut(User),
-}
-
-pub fn start() {
-    /*
     App::builder(init)
         .build_and_start();
-    */
 }
-*/
