e01098fde78e76b626cc65ab378896902e61ee19
Rohan Mishra <rohanmishra2017@gmail.com>
2021-03-31 12:56:37 -0700

    Started building client0-side real time connection

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -4,18 +4,53 @@
 
 use seed::{prelude::*, *};
 
+const CONNECTION_STRING: &str = "";
+
 struct Window {
-    connection: &'a &str,
+    connection: String,
 }
 
-fn init(_: Url) -> Window {
+enum Msg {
+    UplinkEstablish(String),
+    UplinkEstablishSuccessful(String),
+    UplinkEstablishFailed(String),
+    DownlinkEstablish(String),
+    DownlinkEstablishSuccessful(String),
+    DownlinkEstablishFailed(String),
+}
+
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Window {
     // TODO establish real time connection with main server
+    orders.notify(Msg::UplinkEstablish);
+
     Window {
-        connection: "Test"
+        connection: "Test".to_string(),
     }
 }
 
+fn update(msg: Msg, model: &mut Window, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::UplinkEstablish(connection_string) => {
+            println!("Establishing uplink...")
+        },
+        Msg::UplinkEstablishSuccessful(connection_string) => {
+        },
+        Msg::UplinkEstablishFailed(connection_string) => {
+        },
+        Msg::DownlinkEstablish(connection_string) => {
+        },
+        Msg::DownlinkEstablishSuccessful(connection_string) => {
+        },
+        Msg::DownlinkEstablishFailed(connection_string) => {
+        }
+    }
+}
+
+fn view(model: &Window) -> Node<Msg> {
+    h1!["Hello, World!"]
+}
+
+#[wasm_bindgen(start)]
 pub fn start() {
-    App::builder(init)
-        .build_and_start();
+    App::start("app", init, update, view);
 }
