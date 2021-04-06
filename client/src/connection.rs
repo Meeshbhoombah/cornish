91c3037c5882a0470a1bfe1ff1907c0ee7b47dc7
Rohan Mishra <rohanmishra2017@gmail.com>
2021-04-06 12:52:07 -0700

    Started building client real-time connection module

---
+++ client/src/connection.rs
@@ -0,0 +1,24 @@
+/// `connection.rs`
+/// 
+/// Takes in the `mediasoup` object bound the browser's `window` object in
+/// the client's root `index.ts` and creates a useable Rust module such 
+/// that a Webrtc connection can be made from Rust.
+///
+
+/// Reads in a connection string that speceficies the host, port, and,
+/// optionally, path, at which a server connection has been made available to 
+/// the client.
+///
+/// # Arguments
+/// - `connection_url`: a String contatining the `HOST:PORT/PATH` to connect at
+/// 
+/// # Examples
+/// ```
+/// const CONNECTION_STRING = "127.0.0.1:3000/connect";
+/// match let connection::new(CONNECTION_STRING) {
+///     Ok(connection) => {},
+///     Err(err) => {},
+/// }
+/// ```
+pub fn new(connection_url: &str) {}
+
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,26 +2,51 @@
 
 use seed::{prelude::*, *};
 
+// mod connection;
+
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    /*
+    // replace with configuration from environment
+    const SERVER_ADDRESS = "127.0.0.1:3000"
+
+    // TODO send connection::Msg::Establish(SERVER_ADDRESS) after inital render to
+    // create connection with the server
+    */
+
     Model::default()
 }
 
-
 type Model = i32;
 
+/*
+struct Client {
+
+}
+*/
 
 #[derive(Copy, Clone)]
 enum Msg {
     Increment,
 }
 
-
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
         Msg::Increment => *model += 1,
     }
-}
 
+    /*
+    match msg {
+        connection::Msg::Establish => {
+            // TODO take the seerver address and spawn a connection on a new thread
+            // TODO attach this connection to the client
+            match let c = connection::new(SERVER_ADDRESS) {
+                Ok(c)
+                Err(e)
+            }
+        }
+    }
+    */
+}
 
 #[allow(clippy::trivially_copy_pass_by_ref)]
 fn view(model: &Model) -> Node<Msg> {
@@ -31,7 +56,6 @@ fn view(model: &Model) -> Node<Msg> {
         button![model, ev(Ev::Click, |_| Msg::Increment),],
     ]
 }
-
 
 #[wasm_bindgen(start)]
 pub fn start() {
